f5468be8d1807e5f99ae2eca7a04f4b95600d404
Mac <long.lbh@idtinc.co>
2023-04-05 14:05:15 +0700

    fix

--- lib/src/constants/assets_path.dart
+++ lib/src/constants/assets_path.dart
@@ -1,3 +1,5 @@
+part of '../../base_lhe.dart';
+
 class AssetsPath {
   static const String image = "assets/imgs";
   static const String icon = "assets/icons";
---
+++ lib/src/enums/tab_item.dart
@@ -0,0 +1,3 @@
+part of base_lhe;
+
+enum TabItemCode {PRODUCT, ORDER, CUSTOMER, WAREHOUSE}
