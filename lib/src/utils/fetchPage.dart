63c10ab2d690ecca8e7e02efe453254269cc8e84
Mac <long.lbh@idtinc.co>
2023-04-11 15:26:09 +0700

    fix select advance

--- lib/src/utils/fetchPage.dart
+++ lib/src/utils/fetchPage.dart
@@ -9,6 +9,7 @@ class PagingEvent {
 }) async {
   try {
     final newItems = await fetchData(pageKey);
+    print('newItems : $newItems');
     final isLastPage = newItems.length < pageSize;
     if (isLastPage) {
       pagingController.appendLastPage(newItems);
