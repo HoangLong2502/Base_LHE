6b034200199f508dc9002f2c212656ab060bf7b7
Mac <long.lbh@idtinc.co>
2023-04-11 15:18:13 +0700

    fix select advance

--- lib/src/components/base_select_advanced/view.dart
+++ lib/src/components/base_select_advanced/view.dart
@@ -14,7 +14,7 @@ class BaseSelectAdvanced extends StatefulWidget {
   final String? label;
   final String hintSelect;
   final Function? eventSelect;
-  final Function(int)? getListSelect;
+  final Function(int) getListSelect;
 
   @override
   State<BaseSelectAdvanced> createState() => _BaseSelectAdvancedState();
@@ -93,7 +93,7 @@ class _BaseSelectAdvancedState extends State<BaseSelectAdvanced>
       PagingEvent().fetchPage(
         pagingController: _pagingController,
         pageSize: 10,
-        fetchData: widget.getListSelect!,
+        fetchData: widget.getListSelect,
         pageKey: pageKey,
       );
     });
